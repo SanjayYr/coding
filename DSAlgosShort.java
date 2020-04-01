f77de9b53ca7bb2f8ab80e0adabe0cafede34388
Sanjay Y R <sanjayyriit@gmail.com>
2020-04-01 00:58:01 -0700

    Update DSAlgosShort.java

--- DSAlgosShort.java
+++ DSAlgosShort.java
@@ -1,1 +1,17 @@
-// @file: DSAlgos.java
+// @file: DSAlgosShort.java
+
+DFS, BFS runtime -    O(E+V), E is O(V^2)
+  
+Dijkstra and Prims are similar with sptSet(dist[]) and mstSet(keys[])
+
+Kruskal's  - mstSet[], sort edges, detect cycle using Union-Find (By Rank and Path-compression)  
+
+DP - Optimal Sustructure, Overlapping Subproblems
+
+Greedy - Fractional Knapsack, 
+
+3D DP with graphs - https://www.geeksforgeeks.org/shortest-path-exactly-k-edges-directed-weighted-graph/
+
+
+
+
