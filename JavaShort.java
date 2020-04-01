d973c3469af5f891e8a687c426d40e8a0b025cf2
Sanjay Y R <sanjayyriit@gmail.com>
2020-04-01 01:12:44 -0700

    Update JavaShort.java

--- JavaShort.java
+++ JavaShort.java
@@ -1,1 +1,57 @@
 
+// Scanner, BufferedReader, InputStreamReader
+  Scanner sc = new Scanner(System.in);
+  int i = sc.nextInt();
+  String line = sc.nextLine();
+
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  br.nextLine();
+
+// Data types
+boolean, byte, short, int, float, long, double
+Boolean, Byte, Short, Integer, Float, Long, Double
+
+Integer.MAX_VALUE, Integer.MIN_VALUE
+
+// keywords
+static, volatile, synchronized, final, extends, implements, interface, class, enum, abstract class, 
+  
+// Exception handling
+try-catch, throw, throws, finally
+Exception()
+
+// Garbage collection
+System.gc(), Mark and Sweep, finalize(), 
+
+// Threading
+Thread - start(), run(), join(), wait(), notify(),
+Runnable 
+Callable - call(), Future 
+
+// Sort methods
+Arrays.sort() for primitive arrays
+Collection.sort() for ArrayList, LinkedList etc
+Arrays.sort(arr, Collections.reverseOrder())
+Collections.sort(arrList, Collections.reverseOrder())
+
+// Collections
+PriorityQueue
+
+Comparable, Comparator, compare(), compareTo()
+
+Serializable 
+
+ArrayList, LinkedList, List, Arrays, Stack, Queue, ArrayDeque, Map, HashMap, TreeMap, LinkedHashMap
+
+// Object 
+equals(), hashCode(), toString(), 
+
+// String
+String pool
+
+StringBuilder
+
+StringBuffer
+
+
+Set, HashSet, TreeSet, LinkedHashSet, Map.Entry, 
