f3d0f2f6c5feda57625514ae4d8044870a4c8959
Sanjay Y R <sanjayyriit@gmail.com>
2020-04-08 01:25:20 -0700

    Update DSAlgosShort.java

--- DSAlgosShort.java
+++ DSAlgosShort.java
@@ -1,4 +1,42 @@
 // @file: DSAlgosShort.java
+Problem solving patterns:
+
+Two-pointers
+Sliding window
+HashMap for lookups
+HashSet for unique values
+Fast and slow pointers
+Node manipulation
+Binary search
+BFS
+DFS
+Memoization
+Recursion
+Pre, In, Post order traversal of tree
+Recursive and iterative implementations
+Use StringBuilder over String for any string manipulations
+
+Look for iterating from last dependending on situation
+
+Binary tree questions iterative solutions related to sum, we can propagate the value down to leaf.
+
+All combinations of something => backtracking problem
+
+Prefix sum
+XOR
+PriorityQueue for heap related problems
+
+When it comes to shortest step or minimum steps, always have BFS in mind.
+Leetcode 854
+
+For array problems, if the TC > O(n2), try to think through solution by sorting the array
+
+For kth largest or smallest, use median of medians method for O(n) TC and heap method for O(nlogn) TC
+
+For contiguous sub-array problems, try to think in terms of sliding window
+
+If a string involves repeated manipulations, convert into char array and manipulate on the char array 
+
 
 DFS, BFS runtime -    O(E+V), E is O(V^2)
   
